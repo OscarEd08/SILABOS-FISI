c866af19954bf6a9f22e72d182766fa101cc04bb
OscarEd08 <oscar.pairazaman2@unmmsm.edu.pe>
2022-01-21 23:10:23 -0500

    ppipipip

--- main/main.js
+++ main/main.js
@@ -1,5 +1,24 @@
+//Reading JSON
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("../silabos.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
 var pdfID = document.getElementById("pdf");
-var pdf = ["http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf",
+var pdf = ["https://drive.google.com/file/d/12i2f-3EmejtRmgGxeKFFt_HLOqVNwkcA/preview",
             "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf",
             "http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf",
             "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf",
