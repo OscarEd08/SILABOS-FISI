dc1f4abf2e4ee88ea2756d8ab342b90464e5edb6
OscarEd08 <oscar.pairazaman2@unmmsm.edu.pe>
2022-01-21 20:33:42 -0500

    agregando la funcion del pdf

---
+++ main/main.js
@@ -0,0 +1,20 @@
+var pdf = document.getElementById("pdf");
+var curso1 = document.getElementById("curso1");
+var curso2 = document.getElementById("curso2");
+var curso3 = document.getElementById("curso3");
+var curso4 = document.getElementById("curso4");
+var curso5 = document.getElementById("curso5");
+var curso6 = document.getElementById("curso6");
+var curso7 = document.getElementById("curso7");
+
+curso1.onclick = function(){
+    pdf.src = "http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf";
+}
+
+curso2.onclick = function(){
+    pdf.src = "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf";
+}
+
+curso3.onclick = function(){
+    pdf.src = "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf";
+}
