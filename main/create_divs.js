61891ca0f132274ae51f0639e4c240070d3895e8
dzvCode <diego.zavala@unmsm.edu.pe>
2022-01-21 22:32:19 -0500

    gracias Ashel

--- main/create_divs.js
+++
@@ -1,5 +0,0 @@
-var courses = ['curso1','curso2','curso3','curso4','curso5','curso6', 'curso7'];
-
-for(i=0; i<courses.length; i++){
-    $('<div id="' + courses[i] + '" class="list-group-item list-group-item-action pb-4 pt-4">' + courses[i] + '</div>').appendTo('#lista-cursos');
-}
--- main/main.js
+++ main/main.js
@@ -1,5 +1,4 @@
 var pdfID = document.getElementById("pdf");
-var courses = ['curso1','curso2','curso3','curso4','curso5','curso6', 'curso7'];
 var pdf = ["http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf",
             "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf",
             "http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf",
@@ -8,29 +7,13 @@ var pdf = ["http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_g
             "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf",
             "http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf"]
 
-
 //Se crean las listas de los cursos
-for(i=0; i<courses.length; i++){
-    $('<div id="' + courses[i] + '" class="list-group-item list-group-item-action pb-4 pt-4">' + courses[i] + '</div>').appendTo('#lista-cursos');
-    document.getElementById(courses[i])
-    .onclick = function(){
-        pdfID.src = pdf[i];
-    }
+for(i=0; i<7; i++){
+    $('<a id="boton" class="list-group-item list-group-item-action pb-4 pt-4">' + (i+1) + '</a>').appendTo('#lista-cursos');
 }
 
-
-
-//var pdf = document.getElementById("pdf");
-
-/*
-curso1.onclick = function(){
-    pdf.src = "http://www.est.uc3m.es/esp/nueva_docencia/comp_col_leg/ing_tec_inf_gestion/estadistica/Documentacion/Tablas/tablas2caras.pdf";
-}
-
-curso2.onclick = function(){
-    pdf.src = "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf";
-}
-
-curso3.onclick = function(){
-    pdf.src = "http://www.etsii.upm.es/ingor/estadistica/Grado/dTablas.pdf";
-}*/
+var cursos = document.querySelectorAll("#boton")
+console.log(cursos)
+cursos.forEach((curso,index) => curso.onclick = function(){
+    pdfID.src = pdf[index];
+})
